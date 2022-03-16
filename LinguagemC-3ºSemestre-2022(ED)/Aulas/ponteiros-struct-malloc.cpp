e4342a11b2dc9e4b6efe7c2d70d65e683695d61c
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-03-15 21:36:47 -0300

    Atualizações

--- LinguagemC-3ºSemestre-2022(ED)/Aulas/ponteiros-struct-malloc.cpp
+++ LinguagemC-3ºSemestre-2022(ED)/Aulas/ponteiros-struct-malloc.cpp
@@ -19,6 +19,42 @@ int main(){
     *ponteiro = 20; //alterando o valor do ponteiro, altera na variável tmb
 
     printf("\n\nValor de z: %i", z);
+    printf("\n\n------------------\n\n");
+
+    /*ALOCAÇÃO DINÂMICA:
+
+    sizeof -> função que determina o nº de bytes p/ um determinado dado
+        x = sizeof(int); // retorno 4
+
+    malloc -> é uma função que aloca um espaço de memória e retorna um ponteiro do tipo
+    void p/ o inicio do espaço de memoria alocada
+
+    free -> libera o espaço de memória alocado.
+
+    OBS. -->> o (float *) antes do malloc, é para converter o ponteiro p/ o tipo de dado
+    desejado, como a função malloc retorna um ponteiro do tipo VOID, precisamos converter
+    esse ponteiro p/ o tipo da nossa variável, no caso float;*/
+
+    int *v; //tem q ser um ponteiro, pq ele retorna um ponteiro;
+    int i, numero;
+
+    printf("Digite o numero de componentes do vetor: ");
+    scanf("%i", &numero);
+
+    v = (int *)malloc(numero * sizeof(int));
+
+    //armazena
+    for(i = 0; i<numero; i++){
+        printf("Digite os valores: ");
+        scanf("%i", &v[i]);
+    }
+
+    //mostra
+    for(i =0; i<numero; i++){
+        printf("\nValores [%i]: %i", i, v[i]);
+    }
+    free(v);//limpa a memória dela
+
 
     return 0;
 }
