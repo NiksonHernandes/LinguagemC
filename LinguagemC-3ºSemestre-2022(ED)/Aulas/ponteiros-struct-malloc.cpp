ac735121dc9ffe156dba31dab7a389e4baabb955
Nikson Hernandes <79341026+NiksonHernandes@users.noreply.github.com>
2022-03-15 21:10:21 -0300

    Atualizando os exercícios da aula

--- LinguagemC-3ºSemestre-2022(ED)/Aulas/ponteiros-struct-malloc.cpp
+++ LinguagemC-3ºSemestre-2022(ED)/Aulas/ponteiros-struct-malloc.cpp
@@ -3,5 +3,22 @@
 
 int main(){
 
+    //ponteiros:
+    int z = 34;
+    int *ponteiro;
+
+    printf("Valor de z: %i", z);
+    printf("\nEndereço de memória do Z: %x", &z);
+
+    ponteiro = &z;
+
+    printf("\n\nValor alocado no ponteiro: %i", *ponteiro);
+    printf("\nEndereço apontado pelo ponteiro: %x", ponteiro);
+    printf("\nEndereço de memória do ponteiro: %x", &ponteiro);
+
+    *ponteiro = 20; //alterando o valor do ponteiro, altera na variável tmb
+
+    printf("\n\nValor de z: %i", z);
+
     return 0;
 }
